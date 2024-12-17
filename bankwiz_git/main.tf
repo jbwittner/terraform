39eb3e4133ce2b46dc451e00a8b7aca9ca1eaa08
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-12-18 00:34:31 +0100

    :(:(

--- bankwiz_git/main.tf
+++ bankwiz_git/main.tf
@@ -1,4 +1,4 @@
-# Define the variable for GitHub token
+# Define the required variables
 variable "github_token" {}
 variable "sonar_token" {}
 variable "kube_config" {}
@@ -22,31 +22,108 @@ resource "github_repository" "repo" {
   visibility = "public"
 }
 
+# This resource allows you to create and manage branches within your GitHub repository.
 resource "github_branch" "develop" {
   repository = github_repository.repo.name
   branch     = "develop"
   source_branch = "main"
 }
 
+# This resource allows you to set the default branch for your GitHub repository.
 resource "github_branch_default" "branch_default" {
   repository = github_repository.repo.name
   branch     = "develop"
 }
 
+# This resource allows you to create and manage secrets within your GitHub repository.
 resource "github_actions_secret" "sonar_token" {
   repository       = github_repository.repo.name
   secret_name      = "SONAR_TOKEN"
   plaintext_value  = var.sonar_token
 }
 
+# This resource allows you to create and manage secrets within your GitHub repository.
 resource "github_dependabot_secret" "sonar_token" {
   repository       = github_repository.repo.name
   secret_name      = "SONAR_TOKEN"
   plaintext_value  = var.sonar_token
 }
 
+# This resource allows you to create and manage secrets within your GitHub repository.
 resource "github_actions_secret" "kube_config" {
   repository       = github_repository.repo.name
   secret_name      = "KUBE_CONFIG"
   plaintext_value  = var.kube_config
+}
+
+# This resource allows you to create and manage secrets within your GitHub repository.
+resource "github_repository_ruleset" "develop" {
+  name        = "develop"
+  repository  = github_repository.repo.name
+  target      = "branch"
+  enforcement = "active"
+
+  # Define the conditions for the ruleset
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  # Define the rules for the ruleset
+  rules {
+    creation                = true
+    update                  = false
+    deletion                = true
+    required_linear_history = true
+    required_signatures     = false
+    non_fast_forward        = true
+
+    pull_request {
+      required_approving_review_count = 0
+      dismiss_stale_reviews_on_push = true
+      require_code_owner_review = false
+      require_last_push_approval = false
+      required_review_thread_resolution = false
+    }
+
+    required_status_checks {
+      strict_required_status_checks_policy = true
+
+      required_check {
+        context = "Check checkstyle"
+        integration_id = 15368 # GitHub Actions
+      }
+      required_check {
+        context = "Check spotless"
+        integration_id = 15368 # GitHub Actions
+      }
+      required_check {
+        context = "Test domain"
+        integration_id = 15368 # GitHub Actions
+      }
+      required_check {
+        context = "Test application"
+        integration_id = 15368 # GitHub Actions
+      }
+      required_check {
+        context = "Test SPI JPA"
+        integration_id = 15368 # GitHub Actions
+      }
+      required_check {
+        context = "Test API REST"
+        integration_id = 15368 # GitHub Actions
+      }
+      required_check {
+        context = "Build and install application"
+        integration_id = 15368 # GitHub Actions
+      }
+      required_check {
+        context = "Build Docker image (without push)"
+        integration_id = 15368 # GitHub Actions
+      }
+    }
+
+  }
 }
