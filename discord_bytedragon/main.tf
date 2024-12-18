0f66e593f1bd6f3c3e7702666f5fbeda9510eb5e
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-12-18 17:55:58 +0100

    Either Hot Shit or Total Bollocks

--- discord_bytedragon/main.tf
+++ discord_bytedragon/main.tf
@@ -42,12 +42,32 @@ resource "discord_text_channel" "toto_bbb_channel" {
 }
 
 resource "discord_voice_channel" "toto-aaa-voice" {
-  name                     = "toto-aaa-voice"
+  name                     = "💩toto-aaa-voice"
   server_id                = resource.discord_server.bytedragon.id
   category                 = resource.discord_category_channel.toto_category.id
   sync_perms_with_category = true
-  position                 = 1
+  position                 = 0
   user_limit               = 5
+}
+
+resource "discord_role" "moderator" {
+  server_id   = resource.discord_server.bytedragon.id
+  name        = "Moderator"
+  permissions = 8
+  color       = 159
+  hoist       = true
+  mentionable = true
+  position    = 1
+}
+
+resource "discord_member_roles" "jake" {
+  user_id   = 148029630308155392
+  server_id = resource.discord_server.bytedragon.id
+
+  role {
+    role_id = resource.discord_role.moderator.id
+  }
+
 }
 
 resource "discord_message" "hello_world" {
