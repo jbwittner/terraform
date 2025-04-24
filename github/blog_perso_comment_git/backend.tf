508a577a35520678c247ee72638abe6f9f5126c6
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2025-04-24 12:06:25 +0200

    well crap.

---
+++ github/blog_perso_comment_git/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "gcs" {
+    bucket = "f1ca7121a411ff04-terraform-backend"
+    prefix = "terraform/state/github/blog_perso_comment_git"
+  }
+}
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
