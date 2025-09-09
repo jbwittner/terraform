0f99bf45ac20b3782319764756b5c8785bc464fd
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2025-09-09 10:14:46 +0200

    I'm just a grunt. Don't blame me for this awful PoS.

---
+++ github/roue_chance_appro/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "gcs" {
+    bucket = "f1ca7121a411ff04-terraform-backend"
+    prefix = "terraform/state/github/roue_chance_appro_git"
+  }
+}
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
---
+++ github/roue_chance_appro/main.tf
@@ -0,0 +1,24 @@
+# Define the required variables (secrets)
+variable "github_token" {}
+
+# Define the required variables
+
+# Specify the provider
+provider "github" {
+  token = var.github_token # Token sourced from an environment variable
+}
+
+# This resource allows you to create and manage repositories within your GitHub organization or personal account.
+resource "github_repository" "repo" {
+  name                   = "roue_chance_appro"
+  allow_auto_merge       = true
+  allow_merge_commit     = true
+  allow_rebase_merge     = true
+  allow_squash_merge     = true
+  allow_update_branch    = false
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+  gitignore_template     = "Node"
+  auto_init              = true
+  visibility             = "private"
+}
